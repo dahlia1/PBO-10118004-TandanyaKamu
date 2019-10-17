2b834e457416a9ceb4fe24c7cd3395cefc0f7a3c
dahlia1 <55682672+dahlia1@users.noreply.github.com>
2019-10-17 20:33:16 +0700

    Add files via upload

---
+++ PBO-10118004-TandanyaKamu/PBO-10118004-NilaiMahasiswa/src/pbo/pkg10118004/nilaimahasiswa/PBO10118004NilaiMahasiswa.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.pkg10118004.nilaimahasiswa;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ASUS A411UF
+ */
+public class PBO10118004NilaiMahasiswa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Scanner scn1 =  new Scanner(System.in);
+        Scanner scn2 =  new Scanner(System.in);
+        Scanner scn3 =  new Scanner(System.in);
+        
+        Nilai nilai = new Nilai();
+        nilai.getNA();
+        
+        System.out.print("Quiz\t = ");
+        nilai.setQuiz(scn1.nextDouble());
+        System.out.print("UTS\t = ");
+        nilai.setUTS(scn2.nextDouble());
+        System.out.print("UAS\t = ");
+        nilai.setUAS(scn3.nextDouble());
+        
+        System.out.println("\nNilai Akhir "+nilai.nilaiAkhir());
+        System.out.println("\nIndeks = "+nilai.Indeks());
+        System.out.println("Keterangan = "+nilai.aturanKeterangan());
+        
+    }
+    
+}
+
+    }
+    
+}
